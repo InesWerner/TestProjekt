e946f183359acf17f2424d17569dd9885fb06b3a
inesw <inesw@LAPTOP-M3RI453R.fritz.box>
2019-03-26 19:48:25 +0100

    Ausgabe "Hello world, my name is Ines" hinzugefügt

--- ProjektTest_Nummer1/src/Class1.java
+++ ProjektTest_Nummer1/src/Class1.java
@@ -1,12 +1,8 @@
 
 public class Class1 {
 
-	public Class1() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world, my name is Ines!");
 
 	}
 
